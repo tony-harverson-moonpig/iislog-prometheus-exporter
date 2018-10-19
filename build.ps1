c4f507aa047985d72d71a2af52b7a47572b248d9
Tony Harverson <tony.harverson@moonpig.com>
2018-10-19 11:39:01 +0100

    .

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 $binPath = $PSScriptRoot + "/bin"
 $exePath = $binPath + '/IISLogExporter.exe'
-iex 'go get -v'
-iex 'go get gopkg.in/natefinch/lumberjack.v2'
+Invoke-Expression 'go get -v'
+#iex 'go get gopkg.in/natefinch/lumberjack.v2'
 $buildCommand = 'go build  -o ' + $exePath
-iex $buildCommand
+Invoke-Expression $buildCommand
 $copyCommand = 'Copy-Item *.hcl ' + $binPath
-iex $copyCommand
+Invoke-Expression $copyCommand
